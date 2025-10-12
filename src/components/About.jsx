581f5567c4c4f9d630664787be2cf39dea8a732b
Charan394 <charang394@gmail.com>
2025-10-12 11:47:52 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,6 +25,7 @@ const aboutVariants = {
   hidden: { opacity: 0, y: 50 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
 };
+//random changes
 
 const About = () => {
   return (
